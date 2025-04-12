b3db84f67d53e6306629d291b6bb914d53d8ed99
Pauline Moraa <pmoraa431@gmail.com>
2025-04-12 08:35:12 +0300

    Add event listeners to DOM elements in React

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,28 @@
-// Code EyesOnMe Component Here
+// // Code EyesOnMe Component Here
+import React from 'react';
+
+
+function EyesOnMe(){
+    function handleFocus(event){
+        console.log("Good!")
+    }
+    function handleBlur(event){
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        
+            <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+export default EyesOnMe;
+// function EyesOnMe() {
+//     return (
+//       <button
+//         onFocus={() => console.log("Good!")}
+//         onBlur={() => console.log("Hey! Eyes on me!")}
+//       >
+//         Eyes on me
+//       </button>
+//     );
+//   }
+//   export default EyesOnMe;
